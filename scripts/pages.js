c03c96a7acad0fee627666bbd280f04e842773a3
Hurricane <heruokan@gmail.com>
2024-05-26 11:13:05 -0700

    0526_update
    art site _ wip

---
+++ scripts/pages.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+
+
+    $("#int").click(function(){
+        window.location="create/artprojects.html";
+    });
+
+
+    $("#story").click(function(){
+        window.location="create/culturalmsm.html";
+    });
+
+    $("#quillcabin").click(function(){
+        window.location="create/stoneman.html";
+    });
+
+    $("#chamber").click(function(){
+        window.location="create/chamber.html";
+    });
+
+}
