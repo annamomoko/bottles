b17a2544ebb54e63681d7a4d1b761d4628a654a4
Hurricane <heruokan@gmail.com>
2025-12-21 20:35:46 -0800

    update latest art site

---
+++ scripts/pages.js
@@ -0,0 +1,26 @@
+window.onload=function(){
+
+
+    $("#int").click(function(){
+        window.location="create/artprojects.html";
+    });
+
+    $("#fillsea").click(function(){
+        window.location="create/fillthesea.html";
+    });
+
+
+
+    $("#story").click(function(){
+        window.location="create/culturalmsm.html";
+    });
+
+    $("#quillcabin").click(function(){
+        window.location="create/stoneman.html";
+    });
+
+    $("#chamber").click(function(){
+        window.location="create/chamber.html";
+    });
+
+}
